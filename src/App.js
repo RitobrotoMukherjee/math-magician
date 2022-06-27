7f7a7e0a12108317889dae540565f93641ba709a
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-06-27 23:28:59 +0530

    Set up math magician application with CRA

--- src/App.js
+++ src/App.js
@@ -13,14 +13,6 @@ function App() {
           {' '}
           Web App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
