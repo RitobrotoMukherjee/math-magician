599d6b2694665b5328b1ad35f60290f1b9c54e2d
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-07-05 23:01:05 +0530

    Add gh-pages and convert CSS module import to normal CSS import

--- src/components/layouts/Nav.js
+++ src/components/layouts/Nav.js
@@ -1,17 +1,19 @@
 import { Link } from 'react-router-dom';
-import Styles from '../../css/Navigation.module.css';
+import '../../css/Navigation.css';
 
 const Navigation = () => (
   <header>
-    <nav className={Styles.nav}>
-      <div className={`${Styles.logo} Flex-1`}>
-        <Link to="home"><img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Math Magician" /></Link>
+    <nav className="nav">
+      <div className="logo Flex-1">
+        <Link to="home">
+          <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Math Magician" />
+        </Link>
       </div>
-      <ul className={`${Styles.navLinks} Flex-2`}>
-        <li className={Styles.linkItems}>
+      <ul className="navLinks Flex-2">
+        <li className="linkItems">
           <Link to="home">Home</Link>
         </li>
-        <li className={Styles.linkItems}>
+        <li className="linkItems">
           <Link to="calculator">Calculator</Link>
         </li>
       </ul>
